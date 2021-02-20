d4ebe0bc6a81db8915f5d5ed91af873ac51a0693
Baileylin <baileylinhfi@gmail.com>
2021-02-20 18:43:59 -0500

    V1

--- Pre-course Assignment/src/Main.java
+++ Pre-course Assignment/src/Main.java
@@ -14,30 +14,6 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 	}
-	public static boolean binaryIteration(int needle, int[] array) {
-		int middlePosition=array.length/2;
-		int lastPosition=array.length-1;
-		int firstPosition=0;
-		while(firstPosition<=lastPosition) {
-			if(needle>array[middlePosition]) {
-				firstPosition=middlePosition;
-				middlePosition=(lastPosition-firstPosition)/2;
-			}
-			else {
-				lastPosition=middlePosition;
-				middlePosition=(lastPosition-firstPosition)/2;
-			}
-			if(array[middlePosition]==needle)
-			{
-				return true;
-			}
-		}
-		return false;
-	}
 	
-	public static boolean binaryRecursion(int needle, int[] array) {
-		
-		return true;
-	}
 
 }
---
+++ Pre-course Assignment/src/V1.java
@@ -0,0 +1,32 @@
+
+public class V1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static boolean binaryIteration(int needle, int[] array) {
+		int middlePosition=array.length/2;
+		int lastPosition=array.length-1;
+		int firstPosition=0;
+		while(firstPosition<=lastPosition) {
+			if(needle>array[middlePosition]) {
+				firstPosition=middlePosition;
+				middlePosition=(lastPosition-firstPosition)/2;
+			}
+			else {
+				lastPosition=middlePosition;
+				middlePosition=(lastPosition-firstPosition)/2;
+			}
+			if(array[middlePosition]==needle)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static boolean binaryRecursion(int needle, int[] array) {
+		return true;
+	}
+}
